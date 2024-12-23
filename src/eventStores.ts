0cb47adc9986b969814cc19fefc424634fed4c61
olianna <olianna.luxine@foxmail.com>
2024-12-23 15:31:09 +0800

    优化

--- src/eventStores.ts
+++ src/eventStores.ts
@@ -1,5 +1,6 @@
 import { eventEmitter,type Callback } from "./eventEmitter";
 export type EventStoreCallback = () => Record<string, Callback>;
+
 class EventStore {
     readonly eventStoreSign:string;
     private static eventIDArray:string[] = [];
@@ -7,24 +8,25 @@ class EventStore {
         this.eventStoreSign = eventStoreID;
         const _option = option();
         Object.keys(_option).forEach((key)=>{
-            eventEmitter.on(this.eventStoreSign+":"+key,_option[key]);
+            eventEmitter.on(this.eventStoreSign+key,_option[key]);
         })
      }
 
     static createInstance(eventStoreID:string,option:EventStoreCallback): EventStore | null {
-        if (this.eventIDArray.includes(eventStoreID)) {
+        let eventStoreSign =eventStoreID.slice(0,eventStoreID.length-1)
+        if (this.eventIDArray.includes(eventStoreSign)) {
             console.error(`EventStoreError: 标识为${eventStoreID}的仓库重复创建`);
             return null;
         }
-        this.eventIDArray.push(eventStoreID);
+        this.eventIDArray.push(eventStoreSign);
         return new EventStore(eventStoreID,option);
     }
-
     emit(eventName:string,...args:any[]){
-        eventEmitter.emit(this.eventStoreSign+":"+eventName,args);
+        eventEmitter.emit(this.eventStoreSign+eventName,args);
     }
 };
 
+
 export const createEventStore = (eventStoreID:string,option:EventStoreCallback)=>{
-    return EventStore.createInstance(eventStoreID,option);
+    return EventStore.createInstance(eventStoreID+":",option);
 }
